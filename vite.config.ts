36f6bea4a45d69585201a47546217003f0f93c2f
RaamRecords <raam19.music@gmail.com>
2026-05-09 01:49:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
 
-  plugins: [
-    react(),
-    process.env.NODE_ENV === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(process.cwd(), "src"),
     },
   },
 });
